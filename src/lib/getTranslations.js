1016248943b97d8e079653d67776c5a310713612
Splitomania <sebastian-spaeth@gmx.net>
2025-09-25 18:22:36 +0200

    feature: option for fallback translations

--- src/lib/getTranslations.js
+++ src/lib/getTranslations.js
@@ -1,25 +1,30 @@
-export default async (apiKey) => {
+export default async (apiKey, fallback = {}) => {
   if (!apiKey) throw new Error('API key is required')
 
-  const url = window.location.href.includes('http://localhost')
-    ? 'http://localhost:3000'
-    : 'https://api.loqly.dev'
+  let strings = fallback
+  try {
+    const url = window.location.href.includes('http://localhost')
+      ? 'http://localhost:3000'
+      : 'https://api.loqly.dev'
 
-  const response = await fetch(`${url}/v1/strings`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Apikey ${apiKey}`,
-      'Content-Type': 'application/json',
-    },
-  })
+    const response = await fetch(`${url}/v1/strings`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Apikey ${apiKey}`,
+        'Content-Type': 'application/json',
+      },
+    })
 
-  const data = await response.json()
+    const data = await response.json()
 
-  if (!response.ok || data.error) {
-    throw new Error(data.error || 'Something went wrong, please try again.')
-  } else if (data.strings) {
-    return data.strings
-  } else {
-    return {}
+    if (!response.ok || data.error) {
+      throw new Error(data.error || 'Something went wrong, please try again.')
+    } else if (data.strings) {
+      strings = data.strings
+    }
+  } catch (error) {
+    throw new Error(error)
+  } finally {
+    return strings
   }
 }
